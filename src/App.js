6e33e3359b2b68c053c9e20d344e8021d1f299c3
vikash kumar <vkvikashkumar987@gmail.com>
2024-01-23 22:45:16 +0530

    pwa app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+
+  const [dogImageUrl, setDogImageUrl] = useState('');
+
+  useEffect(() => {
+    const apiUrl = 'https://dog.ceo/api/breeds/image/random';
+
+    axios.get(apiUrl)
+      .then(response => {
+        setDogImageUrl(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching dog image:', error.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Dog Image</h1>
+      {dogImageUrl && <img src={dogImageUrl} alt="Random Dog" />}
     </div>
   );
-}
-
+};
 export default App;
